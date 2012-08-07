de93d982b8ab80006a26ec6686dca1fdf5f5b472
John Dewey <john@dewey.ws>
2012-08-06 20:38:13 -0700

    + Init add of lldpd

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "John Dewey"
+maintainer_email "john@dewey.ws"
+license          "Apache 2.0"
+description      "Installs/Configures lldpd"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+recipe           "lldpd", "Installs/Configures lldpd"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
