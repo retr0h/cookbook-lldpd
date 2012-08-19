92b7f5045e263ac2abe3736263130445b6110702
John Dewey <john@dewey.ws>
2012-08-19 15:03:45 -0700

    + qualified constants

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -1,10 +1,10 @@
 describe_recipe "cookbook-lldpd" do
-  include MiniTest::Chef::Assertions
-  include MiniTest::Chef::Context
-  include MiniTest::Chef::Resources
+  include ::MiniTest::Chef::Assertions
+  include ::MiniTest::Chef::Context
+  include ::MiniTest::Chef::Resources
 
   describe "package" do
-    it "installs lldpd" do
+    it "installs" do
       package("lldpd").must_be_installed
     end
   end
