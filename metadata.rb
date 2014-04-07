73faf92bb7fcf4fa7804dcd46da3128f2fc8e6b5
John Dewey <john@dewey.ws>
2014-04-07 10:36:07 -0700

    Made a few rubocop cleanups with new version

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name             'lldpd'
-maintainer       'John Dewey'
+name 'lldpd'
+maintainer 'John Dewey'
 maintainer_email 'john@dewey.ws'
-license          'Apache 2.0'
-description      'Installs/Configures lldpd'
+license 'Apache 2.0'
+description 'Installs/Configures lldpd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version '1.0.1'
 
-recipe           'lldpd', 'Installs/Configures lldpd'
+recipe 'lldpd', 'Installs/Configures lldpd'
 
-%w{ debian ubuntu }.each do |os|
+%w(debian ubuntu).each do |os|
   supports os
 end
